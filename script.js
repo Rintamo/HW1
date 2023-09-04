e3c80cc7b1b07a1038a880e7d3c280039c146fd6
Rintamo <narutoace098@gmail.com>
2023-09-03 23:35:44 -0400

    Added 4 scientific functions

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ function calculate() {
         document.getElementById('display').value = 'Error';
     }
 }
+
+function sqrt() {
+    try {
+        displayValue = Math.sqrt(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function sine() {
+    try {
+        displayValue = Math.sin(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function cosine() {
+    try {
+        displayValue = Math.cos(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function logarithm() {
+    try {
+        displayValue = Math.log(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
